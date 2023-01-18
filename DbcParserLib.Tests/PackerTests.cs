822d653d0d061dc55f3e7fb555298d57f5287824
Taylor Lee <36541914+taylorjdlee@users.noreply.github.com>
2023-01-18 17:31:51 -0500

    Added a 64 bit packing test
    Created a 64 bit packing test to test the newly implemented code

--- DbcParserLib.Tests/PackerTests.cs
+++ DbcParserLib.Tests/PackerTests.cs
@@ -7,7 +7,7 @@ namespace DbcParserLib.Tests
     public class PackerTests
     {
         [Test]
-        public void PackingTest()
+        public void SimplePackingTest()
         {
             var sig = new Signal
             {
@@ -24,6 +24,26 @@ namespace DbcParserLib.Tests
 
             double val = Packer.RxSignalUnpack(TxMsg, sig);
             Assert.AreEqual(-34.3, val, 1e-2);
+        }
+        
+        [Test]
+        public void PackingTest64Bit()
+        {
+            var sig = new Signal
+            {
+                Length = 64,
+                StartBit = 0,
+                IsSigned = 1,
+                ByteOrder = 1, // 0 = Big Endian (Motorola), 1 = Little Endian (Intel)
+                Factor = 1e-16,
+                Offset = 0
+            };
+
+            ulong TxMsg = Packer.TxSignalPack(396.31676720860366, sig);
+            Assert.AreEqual(3963167672086036480, TxMsg);
+
+            double val = Packer.RxSignalUnpack(TxMsg, sig);
+            Assert.AreEqual(396.31676720860366, val);
         }
     }
 }
